438ef209bf4f913801acb5f00f361d6e27d6b16f
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-07-27 11:25:44 +0100

    Add problem and solution for grading students

--- hackerrank-week2/w2.js
+++ hackerrank-week2/w2.js
@@ -10,3 +10,22 @@ function lonelyinteger(a) {
 
     return uniqueArray.filter(num => frequencyMap[num] === 1);
 }
+
+// A2 HackerRank Grading Students
+
+function gradingStudents(grades) {
+    const roundedGrades = [];
+
+    grades.forEach(grade => {
+        if (grade < 38) {
+            roundedGrades.push(grade);
+        } else if (grade % 5 > 2) {
+            grade = grade + 5 - (grade % 5);
+            roundedGrades.push(grade);
+        } else {
+            roundedGrades.push(grade);
+        }
+    });
+
+    return roundedGrades;
+}
