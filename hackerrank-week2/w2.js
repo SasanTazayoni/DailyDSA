d4eb33cdee86de3fafa8bc2c9866f23e4687e6cb
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-07-29 11:24:42 +0100

    Add problem and solution for diagonal difference

--- hackerrank-week2/w2.js
+++ hackerrank-week2/w2.js
@@ -35,3 +35,22 @@ function gradingStudents(grades) {
 function flippingBits(n) {
     return (~n >>> 0);
 }
+
+function diagonalDifference(arr) {
+    let primaryDiagonal = 0
+    let secondaryDiagonal = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i === j) {
+                primaryDiagonal += arr[i][j];
+            }
+
+            if (i + j === arr.length - 1) {
+                secondaryDiagonal += arr[i][j];
+            }
+        }
+    }
+
+    return Math.abs(primaryDiagonal - secondaryDiagonal);
+}
