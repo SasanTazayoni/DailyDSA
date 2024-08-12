462398ccddf9a216b181d277836aab2437b3b979
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-08-12 23:54:52 +0100

    Refactor Picking Numbers solution

--- hackerrank-week4/w4.js
+++ hackerrank-week4/w4.js
@@ -1,18 +1,16 @@
 // A1 Picking Numbers
 
 function pickingNumbers(a) {
-    const frequency = {};
-    for (let num of a) {
-        frequency[num] = (frequency[num] || 0) + 1;
-    }
+    const frequency = new Map();
+
+    a.forEach((num) => frequency.set(num, (frequency.get(num) || 0) + 1));
 
     let maxLength = 0;
 
-    for (let num in frequency) {
-        num = Number(num);
-        const lengthWithNext = frequency[num] + (frequency[num + 1] || 0);
+    frequency.forEach((count, num) => {
+        const lengthWithNext = count + (frequency.get(num + 1) || 0);
         maxLength = Math.max(maxLength, lengthWithNext);
-    }
+    });
 
     return maxLength;
 }
