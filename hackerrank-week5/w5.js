1cb4c7af9e96c11042dcbcdd1e506e8aa4ace361
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-08-23 17:06:17 +0100

    Add problem and solution for Strong Password

--- hackerrank-week5/w5.js
+++ hackerrank-week5/w5.js
@@ -11,3 +11,41 @@ function maxMin(k, arr) {
 
   return minUnfairness;
 }
+
+// A2 Strong password
+
+function minimumNumber(n, password) {
+  const numbers = "0123456789";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const specialCharacters = "!@#$%^&*()-+";
+
+  let hasNumber = false;
+  let hasLower = false;
+  let hasUpper = false;
+  let hasSpecial = false;
+
+  for (let char of password) {
+    if (numbers.includes(char)) {
+      hasNumber = true;
+    } else if (lowerCase.includes(char)) {
+      hasLower = true;
+    } else if (upperCase.includes(char)) {
+      hasUpper = true;
+    } else if (specialCharacters.includes(char)) {
+      hasSpecial = true;
+    }
+
+    if (hasNumber && hasLower && hasUpper && hasSpecial) {
+      break;
+    }
+  }
+
+  let required = 0;
+  if (!hasNumber) required++;
+  if (!hasLower) required++;
+  if (!hasUpper) required++;
+  if (!hasSpecial) required++;
+
+  return Math.max(required, 6 - n);
+}
