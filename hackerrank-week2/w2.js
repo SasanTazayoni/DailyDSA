4bfb1042977e10e97a5afe3d6049115fdde0c582
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-08-24 13:02:02 +0100

    Add solution for Flipping the Matrix

--- hackerrank-week2/w2.js
+++ hackerrank-week2/w2.js
@@ -123,3 +123,24 @@ function marsExploration(s) {
 
   return count;
 }
+
+// Flipping the Matrix
+
+function flippingMatrix(matrix) {
+  const n = matrix.length;
+  let counter = 0;
+
+  for (let i = 0; i < n / 2; i++) {
+    for (let j = 0; j < n / 2; j++) {
+      const maxValue = Math.max(
+        matrix[i][j],
+        matrix[i][n - j - 1],
+        matrix[n - i - 1][j],
+        matrix[n - i - 1][n - j - 1]
+      );
+      counter += maxValue;
+    }
+  }
+
+  return counter;
+}
