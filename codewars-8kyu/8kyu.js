f77fd45365f9d4be9622c436b7136b8bfbc88772
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-08-29 14:41:10 +0100

    Add more Codewars solutions for 8 Kyu

--- codewars-8kyu/8kyu.js
+++ codewars-8kyu/8kyu.js
@@ -20,3 +20,51 @@ function basicOp(operation, value1, value2) {
 function evenOrOdd(number) {
   return number % 2 === 0 ? "Even" : "Odd";
 }
+
+// 3 - Multiply
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// 4 - Return Negative
+
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
+// 5 - Sum of positive
+
+function positiveSum(arr) {
+  return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+}
+
+// 6 - Reversed Strings
+
+function solution(str) {
+  return str.split("").reverse().join("");
+}
+
+// 7 - Converta Number to a String!
+
+function numberToString(num) {
+  return num.toString();
+}
+
+// 8 - Convert boolean values to strings 'Yes' or 'No'.
+
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+// 9 - Opposite number
+
+function opposite(number) {
+  return -number;
+}
+
+// 10 - Remove First and Last Character
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
