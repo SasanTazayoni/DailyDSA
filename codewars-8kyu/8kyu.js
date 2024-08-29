faf42bf3fc16e7b75198a69a0d40a0cd25471b24
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-08-29 13:31:12 +0100

    Add problem and solution Even or Odd

--- codewars-8kyu/8kyu.js
+++ codewars-8kyu/8kyu.js
@@ -14,3 +14,9 @@ function basicOp(operation, value1, value2) {
       return "Invalid operation";
   }
 }
+
+// 2 - Even or Odd
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
