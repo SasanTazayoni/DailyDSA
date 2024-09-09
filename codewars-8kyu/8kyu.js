2ba47a20995370076017a8e0c681b2d070595fcc
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-09-09 13:48:58 +0100

    Add 10 more Codewars solutions for 8 kyu

--- codewars-8kyu/8kyu.js
+++ codewars-8kyu/8kyu.js
@@ -68,3 +68,66 @@ function opposite(number) {
 function removeChar(str) {
   return str.slice(1, -1);
 }
+
+// 11 - Square(n) Sum
+
+function squareSum(numbers) {
+  return numbers.reduce((acc, number) => {
+    return number * number + acc;
+  }, 0);
+}
+
+// 12 - String repeat
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+// 13 - Grasshopper - Summation
+
+const summation = (n) => (n * (n + 1)) / 2; // <-- Clever trick
+
+// 14 - Find the smallest integer in the array
+
+function findSmallestInt(arr) {
+  return Math.min(...arr);
+}
+
+// 15 - Remove String Spaces
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+// 16 - Counting sheep...
+
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
+
+// 17 - Convert a String to a Number!
+
+const stringToNumber = function (str) {
+  return parseInt(str);
+};
+
+// 18 - Abbreviate a Two Word Name
+
+function abbrevName(name) {
+  return name
+    .split(" ")
+    .map((n) => n[0].toUpperCase())
+    .join(".");
+}
+
+// 19 - Century From Year
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+
+// 20 - Keep Hydrated!
+
+function litres(time) {
+  return Math.floor(time / 2);
+}
